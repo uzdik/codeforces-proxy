215770ca1d5cd354df5d44f1327abf208679ca85
Yergali B <yergali@kaist.ac.kr>
2024-05-19 21:03:42 +0900

    Update server.js with recent changes

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://uzdik.github.io');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
     next();
 });
 
