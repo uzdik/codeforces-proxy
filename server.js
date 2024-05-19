da3aba961542169243e8fab30d50b4f26df2d0db
Үздік GitHub <89787851+uzdik@users.noreply.github.com>
2024-05-19 20:35:55 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,73 +1,76 @@
 const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
-const cheerio = require('cheerio');
+const qs = require('querystring');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Enable CORS middleware
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://uzdik.github.io');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+const CODEFORCES_LOGIN_URL = 'https://codeforces.com/enter';
+const CODEFORCES_SUBMIT_URL = 'https://codeforces.com/gym/515622/submit';
 
 // Endpoint to handle submission
 app.post('/submit', async (req, res) => {
-    const { handleOrEmail, password, problemIndex, programTypeId, sourceFileContent, contestId } = req.body;
+    const { handleOrEmail, password, problemIndex, programTypeId, sourceFileContent } = req.body;
 
     try {
-        // Step 1: Log in to Codeforces to get the CSRF token and cookies
-        const loginResponse = await axios.get('https://codeforces.com/enter');
-        const $ = cheerio.load(loginResponse.data);
-        const csrfToken = $('meta[name="X-Csrf-Token"]').attr('content');
-        
-        if (!csrfToken) {
-            throw new Error('Unable to retrieve CSRF token');
-        }
+        // Step 1: Log in to Codeforces
+        let response = await axios.get(CODEFORCES_LOGIN_URL, { withCredentials: true });
+        const cookies = response.headers['set-cookie'].join('; ');
 
-        const cookies = loginResponse.headers['set-cookie'].join('; ');
+        const csrfToken = response.data.match(/name="csrf_token" value="(.*?)"/)[1];
+        const ftaa = response.data.match(/name="ftaa" value="(.*?)"/)[1];
+        const bfaa = response.data.match(/name="bfaa" value="(.*?)"/)[1];
 
-        // Step 2: Authenticate the user
-        await axios.post('https://codeforces.com/enter', {
+        const loginData = qs.stringify({
             handleOrEmail,
             password,
-            csrf_token: csrfToken
-        }, {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Cookie': cookies
-            }
-        });
-
-        // Step 3: Submit the solution
-        const submitResponse = await axios.post(`https://codeforces.com/contest/${contestId}/submit`, {
             csrf_token: csrfToken,
-            submittedProblemIndex: problemIndex,
-            programTypeId: programTypeId,
-            source: sourceFileContent
-        }, {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Cookie': cookies
-            }
+            action: 'enter',
         });
 
-        if (submitResponse.status === 200) {
+        response = await axios.post(CODEFORCES_LOGIN_URL, loginData, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                Cookie: cookies,
+            },
+            withCredentials: true,
+        });
+
+        if (response.status !== 302) {
+            return res.status(400).send('Login failed');
+        }
+
+        // Step 2: Submit the solution
+        const submitData = new FormData();
+        submitData.append('csrf_token', csrfToken);
+        submitData.append('ftaa', ftaa);
+        submitData.append('bfaa', bfaa);
+        submitData.append('action', 'submitSolutionFormSubmitted');
+        submitData.append('submittedProblemIndex', problemIndex);
+        submitData.append('programTypeId', programTypeId);
+        submitData.append('sourceFile', sourceFileContent);
+
+        response = await axios.post(CODEFORCES_SUBMIT_URL, submitData, {
+            headers: {
+                Cookie: cookies,
+                ...submitData.getHeaders(),
+            },
+            withCredentials: true,
+        });
+
+        if (response.status === 200) {
             res.send('Submission successful!');
         } else {
             res.status(400).send('Submission failed');
         }
     } catch (error) {
-        console.error('Error:', error.response ? error.response.data : error.message);
         res.status(500).send('Server error');
     }
 });
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
